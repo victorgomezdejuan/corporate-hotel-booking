1eec2c42ab0f01195e795508f0ffafa3b20c3dfc
Victor Gomez de Juan <victorgomezdejuan@gmail.com>
2023-11-12 12:52:17 +0100

    Descriptive comments added to tests

--- HotelService.Tests/HotelServiceAddHotelTests.cs
+++ HotelService.Tests/HotelServiceAddHotelTests.cs
@@ -7,23 +7,29 @@ public class HotelServiceAddHotelTests
     [Fact]
     public void AddNewHotel()
     {
+        // Arrange
         var hotelRepository = new Mock<IHotelRepository>();
         var hotelService = new HotelService(hotelRepository.Object);
 
+        // Act
         hotelService.AddHotel(1, "Hilton");
 
+        // Assert
         hotelRepository.Verify(x => x.AddHotel(It.Is<Hotel>(h => h.Id == 1 && h.Name == "Hilton")));
     }
 
     [Fact]
     public void AddExistingHotel()
     {
+        // Arrange
         var hotelRepository = new Mock<IHotelRepository>();
         hotelRepository.Setup(x => x.AddHotel(It.IsAny<Hotel>())).Throws<HotelAlreadyExistsException>();
         var hotelService = new HotelService(hotelRepository.Object);
 
+        // Act
         void act() => hotelService.AddHotel(1, "Hilton");
 
+        // Assert
         Assert.Throws<HotelAlreadyExistsException>(act);
     }
 }
