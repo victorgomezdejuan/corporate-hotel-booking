609c8f1c4799de3bdd9c6b367459b8abcd7f2a5d
Victor Gomez de Juan <victorgomezdejuan@gmail.com>
2023-11-12 13:08:06 +0100

    Test setup duplication removed

--- HotelService.Tests/InMemoryHotelRespositoryAddHotelTests.cs
+++ HotelService.Tests/InMemoryHotelRespositoryAddHotelTests.cs
@@ -2,18 +2,24 @@ namespace HotelService.Tests;
 
 public class InMemoryHotelRepositoryAddHotelTests
 {
+    private readonly InMemoryHotelRepository _repository;
+
+    public InMemoryHotelRepositoryAddHotelTests()
+    {
+        _repository = new InMemoryHotelRepository();
+    }
+
     [Fact]
     public void AddNewHotel()
     {
         // Arrange
         var hotel = new Hotel(1, "Hotel 1");
-        var repository = new InMemoryHotelRepository();
         
         // Act
-        repository.AddHotel(hotel);
+        _repository.AddHotel(hotel);
         
         // Assert
-        var actualHotel = repository.GetHotel(1);
+        var actualHotel = _repository.GetHotel(1);
         Assert.Equal(hotel, actualHotel);
     }
 
@@ -22,11 +28,10 @@ public class InMemoryHotelRepositoryAddHotelTests
     {
         // Arrange
         var hotel = new Hotel(1, "Hotel 1");
-        var repository = new InMemoryHotelRepository();
-        repository.AddHotel(hotel);
+        _repository.AddHotel(hotel);
         
         // Act
-        void act() => repository.AddHotel(hotel);
+        void act() => _repository.AddHotel(hotel);
         
         // Assert
         Assert.Throws<HotelAlreadyExistsException>(act);
