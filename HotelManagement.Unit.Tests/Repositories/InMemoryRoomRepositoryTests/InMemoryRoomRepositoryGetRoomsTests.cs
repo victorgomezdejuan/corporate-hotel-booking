8e4033b62755975f6eda7ceaff9bb0d2ff112bc3
Victor Gomez de Juan <victorgomezdejuan@gmail.com>
2023-11-14 14:55:42 +0100

    In-memory Room Repository returns one room if the hotel has one room assigned

--- HotelManagement.Unit.Tests/Repositories/InMemoryRoomRepositoryTests/InMemoryRoomRepositoryGetRoomsTests.cs
+++ HotelManagement.Unit.Tests/Repositories/InMemoryRoomRepositoryTests/InMemoryRoomRepositoryGetRoomsTests.cs
@@ -1,4 +1,5 @@
 using FluentAssertions;
+using HotelManagement.Domain;
 using HotelManagement.Repositories.Rooms;
 
 namespace HotelManagement.Unit.Tests.Repositories.InMemoryRoomRepositoryTests;
@@ -13,5 +14,18 @@ public class InMemoryRoomRepositoryGetRoomsTests
         var rooms = repository.GetRooms(1);
 
         rooms.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void GetOneRoom()
+    {
+        var repository = new InMemoryRoomRepository();
+        repository.AddRoom(new Room(1, 100, RoomType.Double));
+
+        var rooms = repository.GetRooms(1);
+
+        rooms.Should().HaveCount(1);
+        rooms.First().Number.Should().Be(100);
+        rooms.First().Type.Should().Be(RoomType.Double);
     }
 }
