620ec0f172ea86486e78a8b0c5892f00be7ead14
Victor Gomez de Juan <victorgomezdejuan@gmail.com>
2023-11-14 17:59:38 +0100

    Hotel Service does not allow to add the same hotel again

--- HotelManagement.Integrated.Tests/HotelServiceTests.cs
+++ HotelManagement.Integrated.Tests/HotelServiceTests.cs
@@ -1,4 +1,5 @@
 using HotelManagement.Application;
+using HotelManagement.Application.Hotels.Commands.AddHotel;
 using HotelManagement.Repositories.Hotels;
 using HotelManagement.Repositories.Rooms;
 
@@ -7,7 +8,7 @@ namespace HotelManagement.Integrated.Tests;
 public class HotelServiceTests
 {
     [Fact]
-    public void AddHotel()
+    public void AddNewHotel()
     {
         // Arrange
         var hotelRepository = new InMemoryHotelRepository();
@@ -21,5 +22,21 @@ public class HotelServiceTests
         var hotel = hotelService.FindHotel(1);
         Assert.Equal(1, hotel.Id);
         Assert.Equal("Hotel 1", hotel.Name);
+    }
+
+    [Fact]
+    public void AddExistingHotel()
+    {
+        // Arrange
+        var hotelRepository = new InMemoryHotelRepository();
+        var roomRepository = new InMemoryRoomRepository();
+        var hotelService = new HotelService(hotelRepository, roomRepository);
+        hotelService.AddHotel(1, "Hotel 1");
+
+        // Act
+        void Act() => hotelService.AddHotel(1, "Hotel 2");
+
+        // Assert
+        Assert.Throws<HotelAlreadyExistsException>(Act);
     }
 }
