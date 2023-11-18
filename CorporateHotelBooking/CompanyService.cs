02ff215b8e45d19cb2c224ce0b44803668e2b27a
Victor Gomez de Juan <victorgomezdejuan@gmail.com>
2023-11-18 17:22:31 +0100

    Company Service scaffolding added

---
+++ CorporateHotelBooking/CompanyService.cs
@@ -0,0 +1,14 @@
+namespace CorporateHotelBooking;
+
+public class CompanyService
+{
+    public void AddEmployee(int companyId, int employeeId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteEmployee(int employeeId)
+    {
+        throw new NotImplementedException();
+    }
+}
