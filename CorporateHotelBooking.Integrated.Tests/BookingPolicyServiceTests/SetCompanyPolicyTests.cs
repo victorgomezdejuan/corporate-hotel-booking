351b36686150e1951093677cdc0a2908504ca21c
Victor Gomez de Juan <victorgomezdejuan@gmail.com>
2023-11-19 13:13:30 +0100

    Integrated test added to check company policies updates

--- CorporateHotelBooking.Integrated.Tests/BookingPolicyServiceTests/SetCompanyPolicyTests.cs
+++ CorporateHotelBooking.Integrated.Tests/BookingPolicyServiceTests/SetCompanyPolicyTests.cs
@@ -21,4 +21,22 @@ public class SetCompanyPolicyTests
         var retrievedCompanyPolicy = companyPolicyRepository.GetCompanyPolicy(100);
         retrievedCompanyPolicy.Should().Be(companyPolicyToBeAdded);
     }
+
+    [Fact]
+    public void UpdateExistingCompanyPolicy()
+    {
+        // // Arrange
+        // var companyPolicyRepository = new InMemoryCompanyPolicyRepository();
+        // var bookingPolicyService = new BookingPolicyService(companyPolicyRepository);
+        // var addedPolicy = new CompanyPolicy(100, new List<RoomType> { RoomType.Standard, RoomType.JuniorSuite });
+        // bookingPolicyService.SetCompanyPolicy(addedPolicy.CompanyId, addedPolicy.AllowedRoomTypes.ToList());
+        // var updatedPolicy = new CompanyPolicy(100, new List<RoomType> { RoomType.JuniorSuite, RoomType.MasterSuite });
+
+        // // Act
+        // bookingPolicyService.SetCompanyPolicy(updatedPolicy.CompanyId, updatedPolicy.AllowedRoomTypes.ToList());
+
+        // // Assert
+        // var retrievedCompanyPolicy = companyPolicyRepository.GetCompanyPolicy(100);
+        // retrievedCompanyPolicy.Should().Be(updatedPolicy);
+    }
 }
