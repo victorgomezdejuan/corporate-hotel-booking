42452ce3b28c8b49896140312e6519eaac4a6719
Victor Gomez de Juan <victorgomezdejuan@gmail.com>
2023-11-19 18:28:57 +0100

    Booking Policy Service can set policies (can add and update them)

--- CorporateHotelBooking.Integrated.Tests/BookingPolicyServiceTests/SetCompanyPolicyTests.cs
+++ CorporateHotelBooking.Integrated.Tests/BookingPolicyServiceTests/SetCompanyPolicyTests.cs
@@ -9,34 +9,34 @@ public class SetCompanyPolicyTests
     [Fact]
     public void AddNewCompanyPolicy()
     {
-        // // Arrange
-        // var companyPolicyRepository = new InMemoryCompanyPolicyRepository();
-        // var bookingPolicyService = new BookingPolicyService(companyPolicyRepository);
-        // var companyPolicyToBeAdded = new CompanyPolicy(100, new List<RoomType> { RoomType.Standard, RoomType.JuniorSuite  });
+        // Arrange
+        var companyPolicyRepository = new InMemoryCompanyPolicyRepository();
+        var bookingPolicyService = new BookingPolicyService(companyPolicyRepository);
+        var companyPolicyToBeAdded = new CompanyPolicy(100, new List<RoomType> { RoomType.Standard, RoomType.JuniorSuite  });
 
-        // // Act
-        // bookingPolicyService.SetCompanyPolicy(companyPolicyToBeAdded.CompanyId, companyPolicyToBeAdded.AllowedRoomTypes.ToList());
+        // Act
+        bookingPolicyService.SetCompanyPolicy(companyPolicyToBeAdded.CompanyId, companyPolicyToBeAdded.AllowedRoomTypes.ToList());
 
-        // // Assert
-        // var retrievedCompanyPolicy = companyPolicyRepository.GetCompanyPolicy(100);
-        // retrievedCompanyPolicy.Should().Be(companyPolicyToBeAdded);
+        // Assert
+        var retrievedCompanyPolicy = companyPolicyRepository.GetCompanyPolicy(100);
+        retrievedCompanyPolicy.Should().Be(companyPolicyToBeAdded);
     }
 
     [Fact]
     public void UpdateExistingCompanyPolicy()
     {
-        // // Arrange
-        // var companyPolicyRepository = new InMemoryCompanyPolicyRepository();
-        // var bookingPolicyService = new BookingPolicyService(companyPolicyRepository);
-        // var addedPolicy = new CompanyPolicy(100, new List<RoomType> { RoomType.Standard, RoomType.JuniorSuite });
-        // bookingPolicyService.SetCompanyPolicy(addedPolicy.CompanyId, addedPolicy.AllowedRoomTypes.ToList());
-        // var updatedPolicy = new CompanyPolicy(100, new List<RoomType> { RoomType.JuniorSuite, RoomType.MasterSuite });
+        // Arrange
+        var companyPolicyRepository = new InMemoryCompanyPolicyRepository();
+        var bookingPolicyService = new BookingPolicyService(companyPolicyRepository);
+        var addedPolicy = new CompanyPolicy(100, new List<RoomType> { RoomType.Standard, RoomType.JuniorSuite });
+        bookingPolicyService.SetCompanyPolicy(addedPolicy.CompanyId, addedPolicy.AllowedRoomTypes.ToList());
+        var updatedPolicy = new CompanyPolicy(100, new List<RoomType> { RoomType.JuniorSuite, RoomType.MasterSuite });
 
-        // // Act
-        // bookingPolicyService.SetCompanyPolicy(updatedPolicy.CompanyId, updatedPolicy.AllowedRoomTypes.ToList());
+        // Act
+        bookingPolicyService.SetCompanyPolicy(updatedPolicy.CompanyId, updatedPolicy.AllowedRoomTypes.ToList());
 
-        // // Assert
-        // var retrievedCompanyPolicy = companyPolicyRepository.GetCompanyPolicy(100);
-        // retrievedCompanyPolicy.Should().Be(updatedPolicy);
+        // Assert
+        var retrievedCompanyPolicy = companyPolicyRepository.GetCompanyPolicy(100);
+        retrievedCompanyPolicy.Should().Be(updatedPolicy);
     }
 }
