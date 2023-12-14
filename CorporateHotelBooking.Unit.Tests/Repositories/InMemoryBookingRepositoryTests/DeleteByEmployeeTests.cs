0b0e9e2e714ba1c5d5ba3f893b300d010cb53887
Victor Gomez de Juan <victorgomezdejuan@gmail.com>
2023-12-14 15:55:21 +0100

    Booking Repository only deletes the bookings of a specific employee when asked for it

--- CorporateHotelBooking.Unit.Tests/Repositories/InMemoryBookingRepositoryTests/DeleteByEmployeeTests.cs
+++ CorporateHotelBooking.Unit.Tests/Repositories/InMemoryBookingRepositoryTests/DeleteByEmployeeTests.cs
@@ -11,14 +11,30 @@ public class DeleteByEmployeeTests
     {
         // Arrange
         var bookingRepository = new InMemoryBookingRepository();
-        bookingRepository.Add(new Booking(1,1, 1,RoomType.Standard,new DateOnly(2021, 1, 1), new DateOnly(2021, 1, 2)));
-        bookingRepository.Add(new Booking(2,1, 1,RoomType.Standard,new DateOnly(2021, 1, 3), new DateOnly(2021, 1, 4)));
-        bookingRepository.Add(new Booking(3,1, 1,RoomType.Standard,new DateOnly(2021, 1, 5), new DateOnly(2021, 1, 6)));
+        bookingRepository.Add(new Booking(1, 1, 1,RoomType.Standard,new DateOnly(2021, 1, 1), new DateOnly(2021, 1, 2)));
+        bookingRepository.Add(new Booking(2, 1, 1,RoomType.Standard,new DateOnly(2021, 1, 3), new DateOnly(2021, 1, 4)));
+        bookingRepository.Add(new Booking(3, 1, 1,RoomType.Standard,new DateOnly(2021, 1, 5), new DateOnly(2021, 1, 6)));
 
         // Act
         bookingRepository.DeleteByEmployee(1);
 
         // Assert
         bookingRepository.GetCount(1, RoomType.Standard, new DateOnly(2021, 1, 1), new DateOnly(2021, 1, 6)).Should().Be(0);
+    }
+
+    [Fact]
+    public void DeleteNoBookings()
+    {
+        // Arrange
+        var bookingRepository = new InMemoryBookingRepository();
+        bookingRepository.Add(new Booking(1, employeeId: 1, 1,RoomType.Standard,new DateOnly(2021, 1, 1), new DateOnly(2021, 1, 2)));
+        bookingRepository.Add(new Booking(2, employeeId: 1, 1,RoomType.Standard,new DateOnly(2021, 1, 3), new DateOnly(2021, 1, 4)));
+        bookingRepository.Add(new Booking(3, employeeId: 1, 1,RoomType.Standard,new DateOnly(2021, 1, 5), new DateOnly(2021, 1, 6)));
+
+        // Act
+        bookingRepository.DeleteByEmployee(employeeId: 2);
+
+        // Assert
+        bookingRepository.GetCount(1, RoomType.Standard, new DateOnly(2021, 1, 1), new DateOnly(2021, 1, 6)).Should().Be(3);
     }
 }
