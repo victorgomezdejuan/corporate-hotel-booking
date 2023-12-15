c7be520fe04270e63705cd167de9682d550561f5
Victor Gomez de Juan <victorgomezdejuan@gmail.com>
2023-12-15 12:33:04 +0100

    Code cleaning

--- CorporateHotelBooking/Domain/Entities/Hotel.cs
+++ CorporateHotelBooking/Domain/Entities/Hotel.cs
@@ -1,27 +1,15 @@
-using System.Collections.ObjectModel;
-
 namespace CorporateHotelBooking.Domain.Entities;
 
 public class Hotel
 {
-    private readonly List<Room> _rooms = new();
-
     public Hotel(int id, string name)
     {
         Id = id;
         Name = name;
-        _rooms = new List<Room>();
     }
 
     public int Id { get; }
     public string Name { get; }
-
-    public ReadOnlyCollection<Room> Rooms { get => _rooms.AsReadOnly(); }
-
-    public void AddRoom(Room room)
-    {
-        _rooms.Add(room);
-    }
 
     public override bool Equals(object? obj)
     {
