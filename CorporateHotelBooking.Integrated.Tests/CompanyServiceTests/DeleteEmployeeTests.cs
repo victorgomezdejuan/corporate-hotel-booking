dd3ccffc5b86c132821088a18b670347d22e4f68
Victor Gomez de Juan <victorgomezdejuan@gmail.com>
2023-12-15 10:02:46 +0100

    Integrated test that works again uncommented

--- CorporateHotelBooking.Integrated.Tests/CompanyServiceTests/DeleteEmployeeTests.cs
+++ CorporateHotelBooking.Integrated.Tests/CompanyServiceTests/DeleteEmployeeTests.cs
@@ -1,5 +1,6 @@
 using CorporateHotelBooking.Application.Common.Exceptions;
 using CorporateHotelBooking.Repositories.Bookings;
+using CorporateHotelBooking.Repositories.EmployeeBookingPolicies;
 using CorporateHotelBooking.Repositories.Employees;
 using CorporateHotelBooking.Services;
 using FluentAssertions;
@@ -11,19 +12,20 @@ public class DeleteEmployeeTests
     [Fact]
     public void DeleteEmployee()
     {
-        // // Arrange
-        // IEmployeeRepository employeeRepository = new InMemoryEmployeeRepository();
-        // IBookingRepository bookingRepository = new InMemoryBookingRepository();
-        // var companyService = new CompanyService(employeeRepository, bookingRepository);
-        // companyService.AddEmployee(companyId: 100, employeeId: 1);
+        // Arrange
+        IEmployeeRepository employeeRepository = new InMemoryEmployeeRepository();
+        IBookingRepository bookingRepository = new InMemoryBookingRepository();
+        IEmployeeBookingPolicyRepository employeeBookingPolicyRepository = new InMemoryEmployeeBookingPolicyRepository();
+        var companyService = new CompanyService(employeeRepository, bookingRepository, employeeBookingPolicyRepository);
+        companyService.AddEmployee(companyId: 100, employeeId: 1);
 
-        // // Act
-        // companyService.DeleteEmployee(1);
+        // Act
+        companyService.DeleteEmployee(1);
 
-        // // Assert
-        // Action action = () => employeeRepository.Get(1);
-        // action.Should().Throw<EmployeeNotFoundException>();
-        // // TODO: When deleting an employee, all the bookings and policies associated to the employee should also be deleted from the system.
-        // // So far we cannot do it because we haven't implemented the Booking Policy Service and Booking Service.
+        // Assert
+        Action action = () => employeeRepository.Get(1);
+        action.Should().Throw<EmployeeNotFoundException>();
+        // TODO: When deleting an employee, all the bookings and policies associated to the employee should also be deleted from the system.
+        // So far we cannot do it because we haven't implemented the Booking Policy Service and Booking Service.
     }
 }
