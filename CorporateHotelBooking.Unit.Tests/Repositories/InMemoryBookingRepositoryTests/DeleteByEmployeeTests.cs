09c0072043f2b51b39bbeb7710831c49bab8ecf8
Victor Gomez de Juan <victorgomezdejuan@gmail.com>
2023-12-21 13:14:45 +0100

    Test fixed and simplified

--- CorporateHotelBooking.Unit.Tests/Repositories/InMemoryBookingRepositoryTests/DeleteByEmployeeTests.cs
+++ CorporateHotelBooking.Unit.Tests/Repositories/InMemoryBookingRepositoryTests/DeleteByEmployeeTests.cs
@@ -1,50 +1,51 @@
+using AutoFixture.Xunit2;
 using CorporateHotelBooking.Domain.Entities;
 using CorporateHotelBooking.Repositories.Bookings;
+using CorporateHotelBooking.Unit.Tests.Helpers;
 using FluentAssertions;
 
 namespace CorporateHotelBooking.Unit.Tests.Repositories.InMemoryBookingRepositoryTests;
 
 public class DeleteByEmployeeTests
 {
-    [Fact]
-    public void DeleteSeveralBookings()
+    [Theory, AutoData]
+    public void DeleteSeveralBookings(int employeeId, RoomType roomType)
     {
         // Arrange
         var bookingRepository = new InMemoryBookingRepository();
-        bookingRepository.Add(new Booking(1, 1, 1,RoomType.Standard,
-            new DateOnly(2021, 1, 1), new DateOnly(2021, 1, 2)));
-        bookingRepository.Add(new Booking(2, 1, 1,RoomType.Standard,
-            new DateOnly(2021, 1, 3), new DateOnly(2021, 1, 4)));
-        bookingRepository.Add(new Booking(3, 1, 1,RoomType.Standard,
-            new DateOnly(2021, 1, 5), new DateOnly(2021, 1, 6)));
+        var booking1 = bookingRepository.Add(BookingFactory.CreateRandomWithEmployeeAndRoomType(employeeId, roomType));
+        var booking2 = bookingRepository.Add(BookingFactory.CreateRandomWithEmployeeAndRoomType(employeeId, roomType));
+        var booking3 = bookingRepository.Add(BookingFactory.CreateRandomWithEmployeeAndRoomType(employeeId, roomType));
 
         // Act
-        bookingRepository.DeleteByEmployee(1);
+        bookingRepository.DeleteByEmployee(employeeId);
 
         // Assert
-        bookingRepository
-        .GetCount(1, RoomType.Standard, new DateOnly(2021, 1, 1), new DateOnly(2021, 1, 6))
-        .Should().Be(0);
+        // Gettting the booking should throw an exception
+        Action act1 = () => bookingRepository.Get(booking1.Id.Value);
+        act1.Should().Throw<BookingNotFoundException>();
+        Action act2 = () => bookingRepository.Get(booking2.Id.Value);
+        act2.Should().Throw<BookingNotFoundException>();
+        Action act3 = () => bookingRepository.Get(booking3.Id.Value);
+        act3.Should().Throw<BookingNotFoundException>();
     }
 
-    [Fact]
-    public void DeleteNoBookings()
+    [Theory, AutoData]
+    public void DeleteNoBookings(int employeeId, RoomType roomType)
     {
         // Arrange
         var bookingRepository = new InMemoryBookingRepository();
-        bookingRepository.Add(new Booking(1, employeeId: 1, 1,RoomType.Standard,
-            new DateOnly(2021, 1, 1), new DateOnly(2021, 1, 2)));
-        bookingRepository.Add(new Booking(2, employeeId: 1, 1,RoomType.Standard,
-            new DateOnly(2021, 1, 3), new DateOnly(2021, 1, 4)));
-        bookingRepository.Add(new Booking(3, employeeId: 1, 1,RoomType.Standard,
-            new DateOnly(2021, 1, 5), new DateOnly(2021, 1, 6)));
+        var booking1 = bookingRepository.Add(BookingFactory.CreateRandomWithEmployeeAndRoomType(employeeId, roomType));
+        var booking2 = bookingRepository.Add(BookingFactory.CreateRandomWithEmployeeAndRoomType(employeeId, roomType));
+        var booking3 = bookingRepository.Add(BookingFactory.CreateRandomWithEmployeeAndRoomType(employeeId, roomType));
+        var anotherEmployeeId = employeeId + 1;
 
         // Act
-        bookingRepository.DeleteByEmployee(employeeId: 2);
+        bookingRepository.DeleteByEmployee(anotherEmployeeId);
 
         // Assert
-        bookingRepository
-        .GetCount(1, RoomType.Standard, new DateOnly(2021, 1, 1), new DateOnly(2021, 1, 6))
-        .Should().Be(3);
+        bookingRepository.Get(booking1.Id.Value).Should().NotBeNull();
+        bookingRepository.Get(booking2.Id.Value).Should().NotBeNull();
+        bookingRepository.Get(booking3.Id.Value).Should().NotBeNull();
     }
 }
